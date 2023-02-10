b46b61994ac0e05406f8ea6380e73b9e19e7423a
Max L <maxime.lentrebecq@gmail.com>
2023-02-10 12:11:47 +0100

    Last push before hollidays

--- lectureDLL.py
+++ lectureDLL.py
@@ -1,5 +1,6 @@
 import ctypes as ct
 import time
+import copy
 
 # codes d'erreur
 LJV7IF_RC_OK = 0x0000						# Normal termination
@@ -45,6 +46,52 @@ class LJV7IF_PROFILE_INFO(ct.Structure):
 		("IXPitch", ct.c_long)
 	]
 
+class LJV7IF_MEASURE_DATA(ct.Structure):
+	_fields_ = [
+		("byDataInfo", ct.c_byte),
+		("byJudge", ct.c_byte),
+		("reserve", ct.c_byte * 2),
+		("fvalue", ct.c_float)
+	]
+
+class LJV7IF_GET_BATCH_PROFILE_ADVANCE_REQ(ct.Structure):
+	_fields_ = [
+		("byPosMode", ct.c_byte),
+		("reserve", ct.c_byte * 3),
+		("dwGetBatchNo", ct.c_ulong),
+		("dwGetProfNo", ct.c_ulong),
+		("byGetProfCnt", ct.c_byte),
+		("reserve", ct.c_byte * 3)
+	]
+
+class LJV7IF_GET_BATCH_PROFILE_ADVANCE_RSP(ct.Structure):
+	_fields_ = [
+		("dwGetBatchNo", ct.c_ulong),
+		("dwGetBatchProfCnt", ct.c_ulong),
+		("dwGetBatchTopProfNo", ct.c_ulong),
+		("byGetProfCnt", ct.c_byte),
+		("reserve", ct.c_byte * 3)
+	]
+
+class LJV7IF_GET_PROFILE_REQ(ct.Structure):
+	_fields_ = [
+		("byTargetBank", ct.c_byte),
+		("byPosMode", ct.c_byte),
+		("reserve", ct.c_byte * 2),
+		("dwGetProfNo", ct.c_ulong),
+		("byGetProfCnt", ct.c_byte),
+		("byErase", ct.c_byte),
+		("reserve", ct.c_byte * 2)
+	]
+
+class LJV7IF_PROFILE_HEADER(ct.Structure):
+	_fields_ = [
+		("reserve", ct.c_ulong),
+		("dwTriggerCnt", ct.c_ulong),
+		("dwEncoderCnt", ct.c_ulong),
+		("reserve2", ct.c_ulong * 3)
+	]
+
 def Initialize():
 	res = mydll.LJV7IF_Initialize()
 	print(f"Initialisation du DLL : {hex(res)}")
@@ -67,7 +114,7 @@ def HighSpeedDataEthernetCommunicationInitialize(deviceID, ipAddress, port):
 	data = 0
 	def mafonctioncallback(pBuffer, dwSize, dwCount, dwNotify, dwUser):
 		print(pBuffer)
-	prototype = ct.CFUNCTYPE(ct.c_void_p, #ct.byref(ct.c_byte),
+	prototype = ct.CFUNCTYPE(ct.c_void_p, ct.POINTER(ct.c_byte),
 			  ct.c_ulong, ct.c_ulong, ct.c_ulong, ct.c_ulong)
 		
 	Kconnection = LJV7IF_ETHERNET_CONFIG()
@@ -83,6 +130,51 @@ def GetProfile(deviceID, bySendPos, dwDataSize):
 	res = mydll.LJV7IF_GetProfile(deviceID, req, dwDataSize)
 	print("Récupération profile")
 	return res
+
+def GetProfileAdvance(deviceID, dwDataSizeIn):
+	pProfileInfo = LJV7IF_PROFILE_INFO()
+	pdwProfileData = ct.c_ulong * dwDataSizeIn
+	dwDataSize = dwDataSizeIn ###
+	pMeasureData = LJV7IF_MEASURE_DATA()
+
+	res = mydll.LJV7IF_GetProfileAdvance(deviceID, ct.POINTER(pProfileInfo), ct.POINTER(pdwProfileData), dwDataSize, ct.POINTER(pMeasureData))
+	print("Getting profile advance")
+	return res
+
+def GetBatchProfileAdvance(deviceID, dwDataSize):
+	pReq = LJV7IF_GET_BATCH_PROFILE_ADVANCE_REQ()
+	pRsp = LJV7IF_GET_BATCH_PROFILE_ADVANCE_RSP()
+	pProfileInfo = LJV7IF_PROFILE_INFO()
+	pdwBatchData = ct.c_ulong()
+	pBatchMeasureData = LJV7IF_MEASURE_DATA()
+	pMeasureData = LJV7IF_MEASURE_DATA()
+
+	res = mydll.LJV7IF_GetBatchProfileAdvance(deviceID, ct.POINTER(pReq), ct.POINTER(pRsp), ct.POINTER(pProfileInfo), ct.POINTER(pdwBatchData), dwDataSize, ct.POINTER(pBatchMeasureData), ct.POINTER(pMeasureData))
+	return res
+
+def GetProfileValues(profileData, dbData):
+	# Array.Copy(arIntSrcData, (iHEADER_SIZE / sizeof(Int64)), dbValues, 0, iMAX_DATA);
+	dbValues = copy.deepcopy(profileData)
+	for i in range (iMAX_DATA):
+		dbValues[i] = dbSENSOR_RESOLUTION_IN_mm * dbValues[i]
+	return iMAX_DATA
+
+def WriteProfileDataToTextFile(dbValues, iSize, sFileName):
+	f = open(sFileName, "w")
+	
+	dbTemp = 0                                                          # ->intermediate value
+	dbX = 0                                                             # ->x value for profile's point
+	dbStep = 0
+
+	for i in range (iSize):
+		dbStep = ProInf.lXPitch
+		dbX = (ProInf.lXStart + i * dbStep) * dbXResolution
+		dbTemp = dbValues[i] * dbSENSOR_RESOLUTION_IN_mm
+		f.write(str(dbX) + "\t" + str(dbTemp))
+	f.close()
+	
+	return 0
+
 
 def PreStartHighSpeedDataCommunication(deviceID, bySendPos):
 	req = LJV7IF_HIGH_SPEED_PRE_START_REQ()
@@ -137,6 +229,7 @@ ipAddress = "10.2.34.1"
 port = 24691
 portHighSpeed = 24692
 
+
 Initialize()
 GetVersion()
 EthernetOpen(deviceID, ipAddress, port)
@@ -155,3 +248,79 @@ HighSpeedDataCommunicationFinalize(deviceID)
 input("Fin de la sim ? [ENTER]")
 CommClose(deviceID)
 Finalize()
+
+
+dbTravelLength = 0
+dbYStep = 0
+
+iMAX_MEASURE_DATA = 16                        # -> number of output (digital)
+iMAX_DATA = 800                               # -> base length of a profile
+dbSENSOR_RESOLUTION_IN_mm = 1e-5           # -> sensor resolution in mm along Z
+ProInf = LJV7IF_PROFILE_INFO()         # -> information about the curent profile
+dbSENSOR_RESOLUTION_IN_mm_X = 1e-5         # -> sensor resolution in mm along X
+dbXResolution = dbSENSOR_RESOLUTION_IN_mm_X   # ->default resolution along X
+fProfileFileName = "PROFILE_DATA.TXT"      # -> name of the profile file name
+f3DProfileFileName = "PROFILE_DATA_3D.TXT" # -> name of the 3D profile file (RAW)
+iHEADER_SIZE = 24                             # -> data header structure's length ( 6 x sizeof(DWORD))
+iFOOTER_SIZE = 4
+
+
+ 
+# KEYENCE related variables
+iProfileLength = 0                                                     # ->number of points for a single profile
+ProReq = LJV7IF_GET_PROFILE_REQ()
+ProHead = LJV7IF_PROFILE_HEADER()
+meaData = [LJV7IF_MEASURE_DATA() for k in range(iMAX_MEASURE_DATA)]
+
+bKeyenceConnected = False                                         # ->indicates if we are connected to the Keyence controller
+
+
+# initialize the communication with the KEYENCE controller : 
+iProfileLength = iMAX_DATA
+dbData = [0.0 for k in range(iProfileLength)]
+
+# initialize the DLL
+Initialize()
+GetVersion()
+res = EthernetOpen(deviceID, ipAddress, port)
+if res == 0 : bKeyenceConnected = True
+
+
+
+# second step init  
+n = (int)(dbTravelLength / dbYStep)
+
+profileData = [0 for k in range((iMAX_DATA * ct.sizeof(ct.c_int32) + iHEADER_SIZE + iFOOTER_SIZE) / ct.sizeof(ct.c_int))]
+ProReq.byErase = 0
+ProReq.byPosMode = 0
+ProReq.dwGetProfNo = 0
+ProReq.byGetProfCnt = 1; # 1 profile to get ! 
+ProHead.reserve2 = [0, 0, 0]
+
+for k in range(3):
+	for i in range(n):
+		# if we are connected to the Keyence controller, acquire a profile at this location : 
+		if (bKeyenceConnected):
+			# try to read a profile
+			
+			# try to get the data..
+			# if ((rc = NativeMethods.LJV7IF_GetProfileAdvance(DeviceID, ref ProInf, pin.Pointer, iMAX_DATA * sizeof(Int32) + iHEADER_SIZE + iFOOTER_SIZE, meaData)) == (int)(Rc.Ok))
+			dwDataSizeIn = 0
+			if GetProfileAdvance(deviceID, dwDataSizeIn) == 0:
+				# convert the data
+				iProfileLength = GetProfileValues(profileData, dbData)
+				
+				# store the profile measurements into a text file
+				WriteProfileDataToTextFile(dbData, iProfileLength, fProfileFileName)
+				
+				# Appends the data to the 3D data file
+				sOut3DData = open(f3DProfileFileName, "w")
+
+				for j in range (iProfileLength):
+					dbXTemp = 0
+					dbXStep = 0
+					dbXStep = dbXResolution * ProInf.lXPitch
+					dbXStart = ProInf.lXStart * dbXResolution
+					dbXTemp = (j - 1) * dbXResolution + dbXStart
+					sOut3DData.write(str(dbXTemp) + "\t" + str(dbData[j]))
+				sOut3DData.close()
