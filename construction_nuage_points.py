2d5c0438cbcb6aa3893856644877ead34219c04e
Max L <maxime.lentrebecq@gmail.com>
2023-02-21 17:49:21 +0100

    Une belle avancée en cette fin de journée. La bise

--- construction_nuage_points.py
+++ construction_nuage_points.py
@@ -1,48 +1,99 @@
 import os
 import numpy as np
 import csv
+from pykeyence import *
+import math
 
 script_dir = os.path.dirname(__file__)
 file_name_list = ["FaceFlat", "FaceUp"]
 
 points = [[[]]]
+def ceci_est_une_fonction():
+    for file_name in file_name_list :
+        file_path = os.path.join(script_dir, './Faces - Raw CSV/', file_name, '.csv')
+        source_file = open(file_path, "r")
+        file = list(csv.reader(source_file, delimiter = ";"))
 
-for file_name in file_name_list :
-    file_path = os.path.join(script_dir, './Faces - Raw CSV/', file_name, '.csv')
-    source_file = open(file_path, "r")
-    file = list(csv.reader(source_file, delimiter = ";"))
+        nb_lin = len(file)
+        nb_col = len(file[0])
+        print(f"Nombre de colonnes du scan : {nb_col}\nNombre de lignes scannées : {nb_lin}")
 
-    nb_lin = len(file)
-    nb_col = len(file[0])
-    print(f"Nombre de colonnes du scan : {nb_col}\nNombre de lignes scannées : {nb_lin}")
+        file_np = np.asarray(file)
+        file_np = np.char.replace(file_np, ",", ".")
+        file_np = file_np.astype(np.float32)
 
-    file_np = np.asarray(file)
-    file_np = np.char.replace(file_np, ",", ".")
-    file_np = file_np.astype(np.float32)
+        for i in range(nb_lin):
+            for j in range(nb_col):
+                x = i
+                y = j
+                z = file_np[i][j]
 
-    for i in range(nb_lin):
-        for j in range(nb_col):
-            x = i
-            y = j
-            z = file_np[i][j]
+                points.append((x, y, z))
 
-            points.append((x, y, z))
+        # Distance capteur / objet : 200mm
+        # Largeur du faisceau i.e. des nb_col colonnes : 62mm
+        # Supprimer valeurs hors [-48 ; 48]
 
-    # Distance capteur / objet : 200mm
-    # Largeur du faisceau i.e. des nb_col colonnes : 62mm
-    # Supprimer valeurs hors [-48 ; 48]
+        
 
-    
-
-    # ind_min_file_np = np.unravel_index(np.argmin(file_np, axis=None), file_np.shape)
-    # ind_max_file_np = np.unravel_index(np.argmax(file_np, axis=None), file_np.shape)
-    # print(ind_min_file_np, ind_max_file_np)
-    # print(file_np[ind_min_file_np], file_np[ind_max_file_np])
+        # ind_min_file_np = np.unravel_index(np.argmin(file_np, axis=None), file_np.shape)
+        # ind_max_file_np = np.unravel_index(np.argmax(file_np, axis=None), file_np.shape)
+        # print(ind_min_file_np, ind_max_file_np)
+        # print(file_np[ind_min_file_np], file_np[ind_max_file_np])
 
 
-    '''
-    file_path = os.path.join(script_dir, './Faces - TXT/test_a_supp4.txt')
-    fichier_destination = open(file_path, 'w')
-    cw = csv.writer(fichier_destination, delimiter='\t')
-    '''
+        '''
+        file_path = os.path.join(script_dir, './Faces - TXT/test_a_supp4.txt')
+        fichier_destination = open(file_path, 'w')
+        cw = csv.writer(fichier_destination, delimiter='\t')
+        '''
 
+#############################################################################################
+# Script principal
+deviceID = 0
+ipAddress = "10.2.34.1"
+port = 24691
+portHighSpeed = 24692
+
+
+Initialize(debug = True)
+GetVersion()
+EthernetOpen(deviceID, ipAddress, port)
+#a = GetProfileAdvance(deviceID, 800)
+SetSetting_BatchMeasurement(deviceID, 8, SETTING_BATCH_MEASUREMENT_ON)
+SetSetting_SamplingFrequency(deviceID, 8, SETTING_SAMPLING_FREQUENCY_200_HZ)
+SetSetting_TriggerMode(deviceID, 8, SETTING_TRIGGER_MODE_CONTINUOUS)
+
+
+
+exit()
+GetMeasurementValue(deviceID)
+Trigger(deviceID)
+StartMeasure(deviceID)
+StopMeasure(deviceID)
+
+prof_complet_raw = GetBatchProfileAdvance(deviceID)
+print("################ BATCH ################")
+print(prof_complet_raw)
+
+prof_raw = GetProfileAdvance(deviceID)
+prof_clean = [a if a < 1000 else math.inf for a in prof_raw]
+
+print("################ PAS BATCH ################")
+print(prof_clean)
+
+# print("\n".join(map(lambda x: str(x), prof_raw)))
+
+
+
+# déclenchement  continu
+# type : 10h
+# Category : 00h
+# item : 03h
+# target 1~4 : 00h
+# byte 0 : 1
+
+
+input("Fin de la sim ? [ENTER]")
+CommClose(deviceID)
+Finalize()
--- pykeyence.py
+++ pykeyence.py
@@ -27,13 +27,35 @@ dbYStep = 0
 iMAX_MEASURE_DATA = 16                        # -> number of output (digital)
 iMAX_DATA = 800                               # -> base length of a profile
 dbSENSOR_RESOLUTION_IN_mm = 1e-5           # -> sensor resolution in mm along Z
-ProInf = LJV7IF_PROFILE_INFO()         # -> information about the curent profile
+
 dbSENSOR_RESOLUTION_IN_mm_X = 1e-5         # -> sensor resolution in mm along X
 dbXResolution = dbSENSOR_RESOLUTION_IN_mm_X   # ->default resolution along X
 fProfileFileName = "PROFILE_DATA.TXT"      # -> name of the profile file name
 f3DProfileFileName = "PROFILE_DATA_3D.TXT" # -> name of the 3D profile file (RAW)
 iHEADER_SIZE = 24                             # -> data header structure's length ( 6 x sizeof(DWORD))
 iFOOTER_SIZE = 4
+
+SETTING_BATCH_MEASUREMENT_ON = 1				# --> Because it is cute to do it like this
+SETTING_BATCH_MEASUREMENT_OFF = 0
+SETTING_TRIGGER_MODE_CONTINUOUS = 0
+SETTING_TRIGGER_MODE_EXTERNAL = 1
+SETTING_TRIGGER_MODE_ENCODER = 2
+SETTING_SAMPLING_FREQUENCY_10_HZ = 0
+SETTING_SAMPLING_FREQUENCY_20_HZ = 1
+SETTING_SAMPLING_FREQUENCY_50_HZ = 2
+SETTING_SAMPLING_FREQUENCY_100_HZ = 3
+SETTING_SAMPLING_FREQUENCY_200_HZ = 4
+SETTING_SAMPLING_FREQUENCY_500_HZ = 5
+SETTING_SAMPLING_FREQUENCY_1000_HZ = 6
+SETTING_SAMPLING_FREQUENCY_2000_HZ = 7
+SETTING_SAMPLING_FREQUENCY_4000_HZ = 8
+SETTING_SAMPLING_FREQUENCY_4130_HZ = 9
+SETTING_SAMPLING_FREQUENCY_8000_HZ = 10
+SETTING_SAMPLING_FREQUENCY_16000_HZ = 11
+SETTING_SAMPLING_FREQUENCY_32000_HZ = 12
+SETTING_SAMPLING_FREQUENCY_64000_HZ = 13
+
+DEBUG = False
 
 mydll = ct.cdll.LoadLibrary(os.path.join(os.getcwd(), "LJV7_IF.dll")) #"C:/Users/profilometre/Desktop/profilometrie_robot/LJV7_IF.dll")
 
@@ -107,14 +129,28 @@ class LJV7IF_PROFILE_HEADER(ct.Structure):
 		("reserve2", ct.c_ulong * 3)
 	]
 
-def Initialize():
+class LJV7IF_TARGET_SETTING(ct.Structure):
+	_fields_ = [
+		("byType", ct.c_byte),
+		("byCategory", ct.c_byte),
+		("byItem", ct.c_byte),
+		("reserve", ct.c_byte),
+		("byTarget1", ct.c_byte),
+		("byTarget2", ct.c_byte),
+		("byTarget3", ct.c_byte),
+		("byTarget4", ct.c_byte)
+	]
+
+def Initialize(debug = False):
+	global DEBUG
+	DEBUG = debug
 	res = mydll.LJV7IF_Initialize()
-	print(f"Initialisation du DLL : {hex(res)}")
+	if DEBUG: print(f"Initialisation du DLL : {hex(res)}")
 	return res
 
 def GetVersion():
 	res = mydll.LJV7IF_GetVersion()
-	print(f"Version du DLL : {hex(res)}")
+	if DEBUG: print(f"Version du DLL : {hex(res)}")
 	return res
 
 def EthernetOpen(deviceID, ipAddress, port):
@@ -122,8 +158,40 @@ def EthernetOpen(deviceID, ipAddress, port):
 	Kconnection.abyIpAddress[0], Kconnection.abyIpAddress[1], Kconnection.abyIpAddress[2], Kconnection.abyIpAddress[3] = map(lambda x: int(x), ipAddress.split(".", 4))
 	Kconnection.wPortNo = port
 	res = mydll.LJV7IF_EthernetOpen(deviceID, ct.byref(Kconnection))
-	print(f"Connexion à l'adresse IP : {ipAddress}, sur le port {Kconnection.wPortNo} : {hex(res)}")
+	if DEBUG: print(f"Connexion à l'adresse IP : {ipAddress}, sur le port {Kconnection.wPortNo} : {hex(res)}")
 	return res
+
+def SetSetting(deviceID, byType, byCategory, byItem, byTarget1, byTarget2, byTarget3, byTarget4, pData, dwDataSize):
+	byDepth = 1 # 1 pour running, 2 pour sauvegarder dans ROM
+	TargetSetting = LJV7IF_TARGET_SETTING()
+	if DEBUG: print(type(byType))
+	TargetSetting.byType = ct.c_byte(byType)
+	TargetSetting.byCategory = ct.c_byte(byCategory)
+	TargetSetting.byItem = ct.c_byte(byItem)
+	TargetSetting.byTarget1 = ct.c_byte(byTarget1)
+	TargetSetting.byTarget2 = ct.c_byte(byTarget2)
+	TargetSetting.byTarget3 = ct.c_byte(byTarget3)
+	TargetSetting.byTarget4 = ct.c_byte(byTarget4)
+	pdwError = ct.c_ulong()
+
+	mydll.LJV7IF_SetSetting(deviceID, byDepth, TargetSetting, ct.byref(pData), dwDataSize, ct.byref(pdwError))
+	if DEBUG: print(f"Mise à jour du setting de type : {byType}")
+	return pdwError
+
+def SetSetting_TriggerMode(deviceID, program, mode):
+	return SetSetting(deviceID, program + 16, 0, 1, 0, 0, 0, 0, ct.c_byte(mode), 1)
+
+def SetSetting_SamplingFrequency(deviceID, program, freq):
+	return SetSetting(deviceID, program + 16, 0, 2, 0, 0, 0, 0, ct.c_byte(freq), 1)
+
+def SetSetting_BatchMeasurement(deviceID, program, state):
+	return SetSetting(deviceID, program + 16, 0, 3, 0, 0, 0, 0, ct.c_byte(state), 1)
+
+
+
+
+
+
 
 def GetMeasurementValue(deviceID):
 	pMeasureData = (LJV7IF_MEASURE_DATA * 16)()
@@ -139,37 +207,66 @@ def GetProfileAdvance(deviceID):
 	pMeasureData = (LJV7IF_MEASURE_DATA * 16)()
 
 	res = mydll.LJV7IF_GetProfileAdvance(deviceID, ct.byref(pProfileInfo), pdwProfileData, dwDataSize, ct.byref(pMeasureData)) #ct.cast(pMeasureData, ct.POINTER(LJV7IF_MEASURE_DATA))
-	print(f"Getting profile advance : {hex(res)}")
+	if DEBUG: print(f"Getting profile advance : {hex(res)}")
 	return [(pdwProfileData[i] * resolution) for i in range(6, numberOfInt - 1)]
 	#return [(pdwProfileData[i] * resolution) if (pdwProfileData[i] * resolution) < 100 else 0 for i in range(6, numberOfInt - 1)]
 	#return [ct.cast(pdwProfileData, ct.POINTER(ct.c_double))[i] * resolution for i in range(6, numberOfInt - 1)]
 
-def GetBatchProfileAdvance(deviceID, dwDataSize):
+def Trigger(deviceID):
+	res = mydll.LJV7IF_Trigger(deviceID)
+	if DEBUG: print(f"Trigger : {hex(res)}")
+	return res
+
+def StartMeasure(deviceID):
+	res = mydll.LJV7IF_StartMeasure(deviceID)
+	if DEBUG: print(f"Début de la mesure : {hex(res)}")
+	return res
+
+def GetBatchProfileAdvance(deviceID):
+	resolution = 1e-5
+	pProfileInfo = LJV7IF_PROFILE_INFO()
+	numberOfInt = 800 + 6 + 1
+
 	pReq = LJV7IF_GET_BATCH_PROFILE_ADVANCE_REQ()
 	pRsp = LJV7IF_GET_BATCH_PROFILE_ADVANCE_RSP()
-	pProfileInfo = LJV7IF_PROFILE_INFO()
-	pdwBatchData = ct.c_ulong()
+	pdwBatchData = (ct.c_ulong * numberOfInt)()
+	
+	dwDataSize = numberOfInt * ct.sizeof(ct.c_ulong)
+	
 	pBatchMeasureData = LJV7IF_MEASURE_DATA()
 	pMeasureData = LJV7IF_MEASURE_DATA()
 
-	res = mydll.LJV7IF_GetBatchProfileAdvance(deviceID, ct.POINTER(pReq), ct.POINTER(pRsp), ct.POINTER(pProfileInfo), ct.POINTER(pdwBatchData), dwDataSize, ct.POINTER(pBatchMeasureData), ct.POINTER(pMeasureData))
+	res = mydll.LJV7IF_GetBatchProfileAdvance(deviceID, ct.byref(pReq), ct.byref(pRsp), ct.byref(pProfileInfo), ct.byref(pdwBatchData), dwDataSize, ct.byref(pBatchMeasureData), ct.byref(pMeasureData))
+	if DEBUG: print(f"Getting batch profile advance : {hex(res)}")
+	return [(pdwBatchData[i] * resolution) for i in range(numberOfInt)]
+
+def StopMeasure(deviceID):
+	res = mydll.LJV7IF_StopMeasure(deviceID)
+	if DEBUG: print(f"Fin de la mesure : {hex(res)}")
 	return res
 
 def CommClose(deviceID):
 	res = mydll.LJV7IF_CommClose(deviceID)
-	print(f"Fin de la connexion avec device {deviceID} : {hex(res)}")
+	if DEBUG: print(f"Fin de la connexion avec device {deviceID} : {hex(res)}")
 	return res
 
 def Finalize():
 	res = mydll.LJV7IF_Finalize()
-	print(f"Finalisation du DLL : {hex(res)}")
+	if DEBUG: print(f"Finalisation du DLL : {hex(res)}")
 	return res
+
+if __name__ == "__main__":
+	pass
+
+
+
+
 
 '''
 def HighSpeedDataEthernetCommunicationInitialize(deviceID, ipAddress, port):
 	data = 0
 	def mafonctioncallback(pBuffer, dwSize, dwCount, dwNotify, dwUser):
-		print(pBuffer)
+		if DEBUG: print(pBuffer)
 	prototype = ct.CFUNCTYPE(ct.c_void_p, ct.POINTER(ct.c_byte),
 			  ct.c_ulong, ct.c_ulong, ct.c_ulong, ct.c_ulong)
 		
@@ -177,14 +274,14 @@ def HighSpeedDataEthernetCommunicationInitialize(deviceID, ipAddress, port):
 	Kconnection.abyIpAddress[0], Kconnection.abyIpAddress[1], Kconnection.abyIpAddress[2], Kconnection.abyIpAddress[3] = map(lambda x: int(x), ipAddress.split(".", 4))
 	Kconnection.wPortNo = port
 	res = mydll.LJV7IF_HighSpeedDataEthernetCommunicationInitalize(deviceID, ct.byref(Kconnection), prototype(mafonctioncallback))
-	print(f"Initialisation connexion rapide à l'adresse IP : {ipAddress}, sur le port {Kconnection.wPortNo} : {hex(res)}")
+	if DEBUG: print(f"Initialisation connexion rapide à l'adresse IP : {ipAddress}, sur le port {Kconnection.wPortNo} : {hex(res)}")
 	return (res, data)
 
 def GetProfile(deviceID, bySendPos, dwDataSize):
 	req = LJV7IF_HIGH_SPEED_PRE_START_REQ()
 	req.bySendPos = bySendPos
 	res = mydll.LJV7IF_GetProfile(deviceID, req, dwDataSize)
-	print("Récupération profile")
+	if DEBUG: print("Récupération profile")
 	return res
 
 def GetProfileValues(profileData, dbData):
@@ -200,6 +297,8 @@ def WriteProfileDataToTextFile(dbValues, iSize, sFileName):
 	dbTemp = 0                                                          # ->intermediate value
 	dbX = 0                                                             # ->x value for profile's point
 	dbStep = 0
+
+	ProInf = LJV7IF_PROFILE_INFO()         # -> information about the curent profile
 
 	for i in range (iSize):
 		dbStep = ProInf.lXPitch
@@ -217,51 +316,22 @@ def PreStartHighSpeedDataCommunication(deviceID, bySendPos):
 	pProfileInfo = LJV7IF_PROFILE_INFO()
 
 	res = mydll.LJV7IF_PreStartHighSpeedDataCommunication(deviceID, ct.byref(req), ct.byref(pProfileInfo))
-	print(f"Pre start high speed data communication : {hex(res)}")
+	if DEBUG: print(f"Pre start high speed data communication : {hex(res)}")
 
 	return pProfileInfo
 
 def StartHighSpeedDataCommunication(deviceID):
 	res = mydll.LJV7IF_StartHighSpeedDataCommunication(deviceID)
-	print(f"Starting high speed data communication : {hex(res)}")
+	if DEBUG: print(f"Starting high speed data communication : {hex(res)}")
 	return res
 
 def StopHighSpeedDataCommunication(deviceID):
 	res = mydll.LJV7IF_StopHighSpeedDataCommunication(deviceID)
-	print(f"Stoping high speed data communication : {hex(res)}")
+	if DEBUG: print(f"Stoping high speed data communication : {hex(res)}")
 	return res
 
 def HighSpeedDataCommunicationFinalize(deviceID):
 	res = mydll.LJV7IF_HighSpeedDataCommunicationFinalize(deviceID)
-	print(f"Finalizing high speed data communication : {hex(res)}")
-	return res
-
-def StartMeasure(deviceID):
-	res = mydll.LJV7IF_StartMeasure(deviceID)
-	print(f"Début de la mesure : {hex(res)}")
-	return res
-
-def StopMeasure(deviceID):
-	res = mydll.LJV7IF_StopMeasure(deviceID)
-	print(f"Fin de la mesure : {hex(res)}")
+	if DEBUG: print(f"Finalizing high speed data communication : {hex(res)}")
 	return res
 '''
-#############################################################################################
-# Script principal
-deviceID = 0
-ipAddress = "10.2.34.1"
-port = 24691
-portHighSpeed = 24692
-
-Initialize()
-GetVersion()
-EthernetOpen(deviceID, ipAddress, port)
-#a = GetProfileAdvance(deviceID, 800)
-GetMeasurementValue(deviceID)
-a = GetProfileAdvance(deviceID)
-print(a)
-print("\n".join(map(lambda x: str(x), a)))
-
-input("Fin de la sim ? [ENTER]")
-CommClose(deviceID)
-Finalize()
