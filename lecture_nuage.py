b95e0578f829de83956d37383b4c2299392e77f3
Max L <maxime.lentrebecq@gmail.com>
2023-03-02 14:38:17 +0100

    fonctionne

---
+++ lecture_nuage.py
@@ -0,0 +1,31 @@
+import os
+import open3d as o3d
+import numpy as np
+
+FILE_NAME = "nuage.txt"
+SCRIPT_DIR = os.path.dirname(__file__)
+
+file_path = os.path.join(SCRIPT_DIR, FILE_NAME)
+
+# Chargement du nuage de points
+pcd = o3d.io.read_point_cloud(file_path, format = 'xyz')
+
+# Création de la boîte englobante
+bbox = pcd.get_axis_aligned_bounding_box()
+
+# Création de l'objet représentant les axes
+mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size = 16, origin = [0, 0, 0])
+
+# Transformation de la boîte englobante pour représenter les axes
+# mesh_frame.rotate(np.array([[1, 0, 0], [0, -1, 0], [0, 0, -1]]), center = [0, 0, 0])
+# mesh_frame.translate([bbox.max_bound[0], bbox.max_bound[1], bbox.max_bound[2]])
+
+# Création de la fenêtre de visualisation et ajout des géométries
+visualizer = o3d.visualization.Visualizer()
+visualizer.create_window()
+
+visualizer.add_geometry(pcd)
+visualizer.add_geometry(mesh_frame)
+
+# Affichage de la fenêtre de visualisation
+visualizer.run()
