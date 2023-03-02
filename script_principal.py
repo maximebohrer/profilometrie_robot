39676f531f52d60dbd938fbcca069bd1696b5840
Maxime Bohrer <maxime21.bohrer@gmail.com>
2023-03-02 13:17:46 +0100

    ok

--- script_principal.py
+++ script_principal.py
@@ -1,9 +1,57 @@
 import time
 from pykuka import *
 from pykeyence import *
+# import open3d as o3d
+import os
 
-depart = Pose(0, 0, -10)
-arrivee = Pose(0, 100, -10)
+DEVICE_ID   = 0
+IP_ADDRESS  = "10.2.34.1"
+PORT        = 24691
+PROGRAM     = 8
+FILE_NAME   = "nuage.txt"
 
-go_to_pose(depart)
-go_to_pose(arrivee)
+Initialize(debug = True)
+GetVersion()
+EthernetOpen(DEVICE_ID, IP_ADDRESS, PORT)
+
+#send_3964R_single_char(GO)
+
+go = read_3964R_single_char()
+
+if go != GO: exit()
+
+pose = read_3964R_pose()
+print(f"Position de départ : {pose}")
+
+StartMeasure(DEVICE_ID)
+
+done = read_3964R_single_char()
+
+while done != DONE:
+    done = read_3964R_single_char()
+
+StopMeasure(DEVICE_ID)
+
+
+NB_PROF_PAR_LOT = 10
+LIM_NB_PROF = 1000
+yStep = 0.1
+
+cloud = GetBatchProfileAdvance(DEVICE_ID, NB_PROF_PAR_LOT, LIM_NB_PROF, yStep)
+
+
+# Visualisation 3D du profile
+FILE_NAME = "nuage.txt"
+SCRIPT_DIR = os.path.dirname(__file__)
+
+f = open(FILE_NAME, 'w')
+for item in cloud:
+    f.write(str(item[0]) + "\t" + str(item[1]) + "\t" + str(item[2]) + "\n")
+f.close()
+
+# file_path = os.path.join(SCRIPT_DIR, FILE_NAME)
+# pcd = o3d.io.read_point_cloud(file_path, format = 'xyz')
+# o3d.visualization.draw_geometries([pcd])
+
+CommClose(DEVICE_ID)
+Finalize()
