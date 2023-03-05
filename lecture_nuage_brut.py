f8dccb9524edbaf677c08242cb355f1a96367a3a
maximebohrer <maxime21.bohrer@gmail.com>
2023-03-06 00:11:09 +0100

    amélioration calibration

--- lecture_nuage_brut.py
+++ lecture_nuage_brut.py
@@ -1,12 +1,12 @@
 import os
 import numpy as np
 from pykuka import Pose
-import transformations as trans
+import transformations as tf
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 from mpl_toolkits.mplot3d import Axes3D
 
-f_brut = open("nuage_brut.txt", 'r')
+f_brut = open("data/nuage_brut.txt", 'r')
 f = open("data/nuage.txt", 'w')
 
 class Face:
@@ -28,7 +28,7 @@ while line != "":
 
     while line != "**********":
         split = line.split("\t")
-        if i % 100 == 0:
+        if i % 50 == 0:
             face.points.append([float(split[0]), float(split[1]), float(split[2])])
         i += 1
         line = f_brut.readline().strip()
@@ -37,17 +37,6 @@ while line != "":
 
 f.close()
 
-def calcul_des_faces(faces, x, y, z, a, b, c):
-    base_point_cloud_dans_base_profilo = trans.get_htm(x, y, z, a, b, c)
-    cube = np.empty((0,3))
-    for f in faces:
-        base_outil_dans_base_profilo = trans.get_htm(f.x, f.y, f.z, f.a, f.b, f.c)
-        base_profilo_dans_base_outil = base_outil_dans_base_profilo.I
-        base_point_cloud_dans_base_outil = base_profilo_dans_base_outil * base_point_cloud_dans_base_profilo
-        points_dans_base_outil = trans.apply_htm(base_point_cloud_dans_base_outil, f.points)
-        cube = np.append(cube, points_dans_base_outil, axis=0)
-    return cube
-
 x_ini = -58
 y_ini = -8.7
 z_ini = -168.58
@@ -55,12 +44,6 @@ a_ini = -5.02
 b_ini = -3.46
 c_ini = 0.1
 epsylon = 10
-
-cube = calcul_des_faces(faces, x_ini, y_ini, z_ini, a_ini, b_ini, c_ini)
-
-# fix, ax = plt.subplots()
-# line, = ax.plot(cube, lw=2)
-# ax.set_xlabel('Time [s]')
 
 # créer une figure 3D
 fig = plt.figure()
@@ -74,47 +57,63 @@ ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 
-# créer le nuage de points
-graph = ax.scatter(cube[:, 0], cube[:, 1], cube[:, 2], marker='.', s=1)
+def draw_graph(x, y, z, a, b, c):
+    # calculate cube
+    base_point_cloud_dans_base_profilo = tf.get_htm(x, y, z, a, b, c)
+    cube = np.empty((0,3))
+    for f in faces:
+        base_outil_dans_base_profilo = tf.get_htm(f.x, f.y, f.z, f.a, f.b, f.c)
+        base_profilo_dans_base_outil = base_outil_dans_base_profilo.I
+        base_point_cloud_dans_base_outil = base_profilo_dans_base_outil * base_point_cloud_dans_base_profilo
+        f.points_dans_base_outil = tf.apply_htm(base_point_cloud_dans_base_outil, f.points)
+        cube = np.append(cube, f.points_dans_base_outil, axis=0)
 
-# Make a horizontal slider to control x.
+    # draw cube
+    colors = ["red", "green", "blue", "darkorange", "purple", "darkcyan"]
+    ax.clear()
+    for i, f in enumerate(faces):
+        ax.scatter(f.points_dans_base_outil[:, 0], f.points_dans_base_outil[:, 1], f.points_dans_base_outil[:, 2], marker='.', s=2, c=colors[i % len(colors)])
+    ax.set_box_aspect(list(map(lambda lim3d: lim3d[1] - lim3d[0], [ax.get_xlim3d(), ax.get_ylim3d(), ax.get_zlim3d()])))
+    fig.canvas.draw_idle()
+
+draw_graph(x_ini, y_ini, z_ini, a_ini, b_ini, c_ini)
+
 axis_x = fig.add_axes([0.25, 0.16, 0.65, 0.03])
-x_slider = Slider(ax=axis_x,    label='x [mm]',   valmin=x_ini - epsylon, valmax=x_ini + epsylon,  valinit=x_ini)
-
-# Make a horizontal slider to control y.
 axis_y = fig.add_axes([0.25, 0.13, 0.65, 0.03])
-y_slider = Slider(ax=axis_y,    label='y [mm]',   valmin=y_ini - epsylon, valmax=y_ini + epsylon,  valinit=y_ini)
-
-# Make a horizontal slider to control z.
-axis_z = fig.add_axes([0.25, 0.100, 0.65, 0.03])
-z_slider = Slider(ax=axis_z,    label='z [mm]',   valmin=z_ini - epsylon, valmax=z_ini + epsylon,  valinit=z_ini)
-
-# Make a horizontal slider to control a.
+axis_z = fig.add_axes([0.25, 0.10, 0.65, 0.03])
 axis_a = fig.add_axes([0.25, 0.07, 0.65, 0.03])
-a_slider = Slider(ax=axis_a,    label='a [°]',   valmin=a_ini - epsylon, valmax=a_ini + epsylon,  valinit=a_ini)
-
-# Make a horizontal slider to control b.
 axis_b = fig.add_axes([0.25, 0.04, 0.65, 0.03])
-b_slider = Slider(ax=axis_b,    label='b [°]',   valmin=b_ini - epsylon, valmax=b_ini + epsylon,  valinit=b_ini)
-
-# Make a horizontal slider to control c.
 axis_c = fig.add_axes([0.25, 0.01, 0.65, 0.03])
-c_slider = Slider(ax=axis_c,    label='c [°]',   valmin=c_ini - epsylon, valmax=c_ini + epsylon,  valinit=c_ini)
+
+def create_sliders():
+    global x_slider, y_slider, z_slider, a_slider, b_slider, c_slider
+
+    axis_x.clear()
+    axis_y.clear()
+    axis_z.clear()
+    axis_a.clear()
+    axis_b.clear()
+    axis_c.clear()
+
+    x_slider = Slider(ax=axis_x,    label='x [mm]',   valmin=x_ini - epsylon,   valmax=x_ini + epsylon,   valinit=x_ini)
+    y_slider = Slider(ax=axis_y,    label='y [mm]',   valmin=y_ini - epsylon,   valmax=y_ini + epsylon,   valinit=y_ini)
+    z_slider = Slider(ax=axis_z,    label='z [mm]',   valmin=z_ini - epsylon,   valmax=z_ini + epsylon,   valinit=z_ini)
+    a_slider = Slider(ax=axis_a,    label='a [°]',    valmin=a_ini - epsylon,   valmax=a_ini + epsylon,   valinit=a_ini)
+    b_slider = Slider(ax=axis_b,    label='b [°]',    valmin=b_ini - epsylon,   valmax=b_ini + epsylon,   valinit=b_ini)
+    c_slider = Slider(ax=axis_c,    label='c [°]',    valmin=c_ini - epsylon,   valmax=c_ini + epsylon,   valinit=c_ini)
+
+    x_slider.on_changed(update)
+    y_slider.on_changed(update)
+    z_slider.on_changed(update)
+    a_slider.on_changed(update)
+    b_slider.on_changed(update)
+    c_slider.on_changed(update)
 
 # The function to be called anytime a slider's value changes
 def update(val):
-    ax.clear()
-    cube = calcul_des_faces(faces, x_slider.val, y_slider.val, z_slider.val, a_slider.val, b_slider.val, c_slider.val)
-    ax.scatter(cube[:, 0], cube[:, 1], cube[:, 2], marker='.', s=1)
-    fig.canvas.draw_idle()
+    draw_graph(x_slider.val, y_slider.val, z_slider.val, a_slider.val, b_slider.val, c_slider.val)
 
-# register the update function with each slider
-x_slider.on_changed(update)
-y_slider.on_changed(update)
-z_slider.on_changed(update)
-a_slider.on_changed(update)
-b_slider.on_changed(update)
-c_slider.on_changed(update)
+create_sliders()
 
 # Create a button to reset the sliders to initial values.
 resetax = fig.add_axes([0.8, 0.20, 0.1, 0.04])
@@ -133,54 +132,13 @@ button_reset.on_clicked(f_reset)
 # Create a button to increase the precision reset the sliders to initial values.
 augmenter_precision = fig.add_axes([0.8, 0.24, 0.1, 0.04])
 button_augmenter_precision = Button(augmenter_precision, 'Up', hovercolor='0.975')
-
-def upadate_los_cursores():
-    global x_ini, y_ini, z_ini, a_ini, b_ini, c_ini
-    global x_slider, y_slider, z_slider, a_slider, b_slider, c_slider
-    global epsylon
-    
-    x_slider.val = x_ini
-    x_slider.valinit = x_slider.val
-    x_slider.valmin, x_slider.valmax = x_ini - epsylon, x_ini + epsylon
-    x_slider.ax.set_xlim(x_slider.valmin, x_slider.valmax)
-    x_slider.set_val(x_ini)
-
-    y_slider.val = y_ini
-    y_slider.valinit = y_slider.val
-    y_slider.valmin, y_slider.valmax = y_ini - epsylon, y_ini + epsylon
-    y_slider.ax.set_xlim(y_slider.valmin, y_slider.valmax)
-    y_slider.set_val(y_ini)
-
-    z_slider.val = z_ini
-    z_slider.valinit = z_slider.val
-    z_slider.valmin, z_slider.valmax = z_ini - epsylon, z_ini + epsylon
-    z_slider.ax.set_xlim(z_slider.valmin, z_slider.valmax)
-    z_slider.set_val(z_ini)
-
-    a_slider.val = a_ini
-    a_slider.valinit = a_slider.val
-    a_slider.valmin, a_slider.valmax = a_ini - epsylon, a_ini + epsylon
-    a_slider.ax.set_xlim(a_slider.valmin, a_slider.valmax)
-    a_slider.set_val(a_ini)
-
-    b_slider.val = b_ini
-    b_slider.valinit = b_slider.val
-    b_slider.valmin, b_slider.valmax = b_ini - epsylon, b_ini + epsylon
-    b_slider.ax.set_xlim(b_slider.valmin, b_slider.valmax)
-    b_slider.set_val(b_ini)
-
-    c_slider.val = c_ini
-    c_slider.valinit = c_slider.val
-    c_slider.valmin, c_slider.valmax = c_ini - epsylon, c_ini + epsylon
-    c_slider.ax.set_xlim(c_slider.valmin, c_slider.valmax)
-    c_slider.set_val(c_ini)
     
 def f_augmenter_precision(event):
-    f_sauvegarder_valeurs("")
     global epsylon
-    if epsylon > 1 :    epsylon -= 1
-    else :              print("[f_augmenter_precision] Précision maximale déjà atteinte")
-    upadate_los_cursores()
+    global x_ini, y_ini, z_ini, a_ini, b_ini, c_ini
+    x_ini, y_ini, z_ini, a_ini, b_ini, c_ini = x_slider.val, y_slider.val, z_slider.val, a_slider.val, b_slider.val, c_slider.val
+    epsylon /= 2
+    create_sliders()
 
 button_augmenter_precision.on_clicked(f_augmenter_precision)
 
@@ -189,10 +147,11 @@ diminuer_precision = fig.add_axes([0.8, 0.28, 0.1, 0.04])
 button_diminuer_precision = Button(diminuer_precision, 'Down', hovercolor='0.975')
 
 def f_diminuer_precision(event):
-    f_sauvegarder_valeurs("")
     global epsylon
-    epsylon += 1
-    upadate_los_cursores()
+    global x_ini, y_ini, z_ini, a_ini, b_ini, c_ini
+    x_ini, y_ini, z_ini, a_ini, b_ini, c_ini = x_slider.val, y_slider.val, z_slider.val, a_slider.val, b_slider.val, c_slider.val
+    epsylon *= 2
+    create_sliders()
 
 button_diminuer_precision.on_clicked(f_diminuer_precision)
 
@@ -203,6 +162,7 @@ button_sauvegarder_valeurs = Button(sauvegarder_valeurs, 'Save', hovercolor='0.9
 def f_sauvegarder_valeurs(event):
     global x_ini, y_ini, z_ini, a_ini, b_ini, c_ini
     x_ini, y_ini, z_ini, a_ini, b_ini, c_ini = x_slider.val, y_slider.val, z_slider.val, a_slider.val, b_slider.val, c_slider.val
+    create_sliders()
 
 button_sauvegarder_valeurs.on_clicked(f_sauvegarder_valeurs)
 
--- transformations.py
+++ transformations.py
@@ -26,24 +26,36 @@ def Rz(a: float):
                       [0,         0,          1]])
 
 def get_htm(x: float, y: float, z: float, a: float, b: float, c: float) -> np.matrix:
-    """Get the homogenous transformation matrix from the x, y, z, a, b, c of the robot, x, y and z are in mm, a, b and c in degrees."""
-    rotation_matrix = Rz(np.radians(a)) * Ry(np.radians(b)) * Rx(np.radians(c))                              # this expression depends on the robot (alpha = a, beta = b, gamma = c)
-    return np.vstack([np.hstack([rotation_matrix, np.array([[x], [y], [z]])]), np.array([[0, 0, 0, 1]])])    # construction of the homogenous transformation matrix
+    """Get the homogeneous transformation matrix from the x, y, z, a, b, c of the robot. x, y and z are in mm; a, b and c in degrees. It is used to go from tool coordinates to base coordinates for example.
+    The matrix can then be inverted (using the .I method) to represent the reverse transformation (base coordinates to tool coordinates for example), or multiplied together to chain transmormations.
+    When multiplying matrices together, the last matrix of the product is the first transformation that is applied."""
+    a, b, c = np.radians(a), np.radians(b), np.radians(c)
+    rotation_matrix = Rz(a) * Ry(b) * Rx(c)                              # this expression depends on the robot (alpha = a, beta = b, gamma = c)
+    return np.vstack([np.hstack([rotation_matrix, np.array([[x], [y], [z]])]), np.array([[0, 0, 0, 1]])])    # construction of the homogeneous transformation matrix
 
 def apply_htm(htm: np.matrix, points: np.ndarray) -> np.ndarray:
-    """Apply the homogenous transformation matrix to a list of points."""
+    """Apply the homogeneous transformation matrix to a list of points.
+    This function is used to handle the array manipulations, since a fourth coordinate (1) needs to be added to a 3D point before multiplying it by a homogeneous transformation matrix."""
     transpose = np.matrix(points).T                                    # transpose the array so that the points are columns
     with_ones = np.vstack([transpose, np.ones(transpose.shape[1])])    # adding a row of ones for the next calculation
-    res = htm * with_ones                                              # applying the homogenous transformation matrix to the list of points
+    res = htm * with_ones                                              # applying the homogeneous transformation matrix to the list of points
     return np.array(np.delete(res, 3, axis=0).T)                       # remove the last row and transpose again
 
 if __name__=="__main__":
-    x = -0.81
-    y = 501.10
-    z = 499.84
-    a = -169.5
-    b = 50.62
-    c = 95.50
-    mat = get_htm(x, y, z, a, b, c)
-    point_in_world = apply_htm(mat, np.array([0,10,100]))
-    print(point_in_world)
+    # x = -0.81
+    # y = 501.10
+    # z = 499.84
+    # a = -169.5
+    # b = 50.62
+    # c = 95.50
+    # mat = get_htm(x, y, z, a, b, c)
+    # point_in_world = apply_htm(mat, np.array([0,10,100]))
+    # print(point_in_world)
+
+    base2_dans_base1 = get_htm(2,4,9,40,20,15)
+    base1_dans_base0 = get_htm(1,2,3,10,15,20)
+    base2_dans_base0 = base1_dans_base0 * base2_dans_base1
+
+    print(base2_dans_base0)
+    print()
+    print(get_htm(2+1, 4+2, 9+3, 40+10, 20+15, 15+20))
